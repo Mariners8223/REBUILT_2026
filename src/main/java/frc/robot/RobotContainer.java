72e7f5de19d574ea481f34c0908e7f186d687d4e
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2026-01-18 14:39:53 +0200

    litterly everything

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,71 @@
+package frc.robot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BooleanSupplier;
+
+import com.pathplanner.lib.events.EventTrigger;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.*;
+import frc.robot.Constants.RobotType;
+import frc.robot.commands.Drive.DriveCommand;
+
+import org.json.simple.parser.ParseException;
+import org.littletonrobotics.conduit.ConduitApi;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+import frc.robot.subsystems.DriveTrain.DriveBase;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+    public static DriveBase driveBase;
+
+    public static CommandPS5Controller driveController;
+    public static CommandXboxController driveXboxController;
+
+
+    public RobotContainer() {
+        driveController = new CommandPS5Controller(1);
+        driveXboxController = new CommandXboxController(0);
+
+        driveBase = new DriveBase();
+
+        configureDriveBindings();
+        
+    }
+
+
+    public void configureDriveBindings(){
+        new Trigger(RobotState::isTeleop).and(RobotState::isEnabled).whileTrue(new StartEndCommand(() ->
+            driveBase.setDefaultCommand(new DriveCommand(driveBase, driveXboxController)),
+            driveBase::removeDefaultCommand).ignoringDisable(true));
+   }
+   
+
+
+}
