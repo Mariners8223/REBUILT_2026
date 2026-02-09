a547fe60fa56c730be9cccfb95d1876dd01f52a4
FrcIroniH <eyal.birger2010@gmail.com>
2026-02-09 13:16:52 +0200

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,12 +24,6 @@ public class RobotContainer {
     public RobotContainer() {
         driveController = new CommandPS5Controller(1);
         driveXboxController = new CommandXboxController(0);
-<<<<<<< Updated upstream
-=======
-        driveBase = new DriveBase();
-        isuff = new isuff();
-        configureDriveBindings();
->>>>>>> Stashed changes
 
         driveBase = new DriveBase();
 
@@ -39,18 +33,10 @@ public class RobotContainer {
 
 
     public void configureDriveBindings(){
-<<<<<<< Updated upstream
         new Trigger(RobotState::isTeleop).and(RobotState::isEnabled).whileTrue(new StartEndCommand(() ->
             driveBase.setDefaultCommand(new DriveCommand(driveBase, driveXboxController)),
             driveBase::removeDefaultCommand).ignoringDisable(true));
-=======
-       new Trigger(RobotState::isTeleop).and(RobotState::isEnabled).whileTrue(new StartEndCommand(() ->
-       driveBase.setDefaultCommand(new DriveCommand(driveBase, driveXboxController)),
-       driveBase::removeDefaultCommand).ignoringDisable(true));
-       driveController.options().onTrue(driveBase.resetOnlyDirection());
-      
-       driveXboxController.y().whileTrue(new MoveIsuff(isuff)).whileFalse(new Stopisuff(isuff));
->>>>>>> Stashed changes
+
    }
    
 
