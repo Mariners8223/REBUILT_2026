bc8bb0c304f199db8a6a484eac7b85a9d50e6589
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2026-02-19 14:19:22 +0200

    added swerve placments to main

--- src/main/java/frc/robot/subsystems/DriveTrain/DriveBaseConstants.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/DriveBaseConstants.java
@@ -9,10 +9,10 @@ import frc.robot.subsystems.DriveTrain.SwerveModules.DevBotConstants;
 import frc.util.PIDFGains;
 
 public class DriveBaseConstants {
-
+        
     ///public static final double DISTANCE_BETWEEN_WHEELS = 0.58; // the distance between each wheel in meters
-    public static final double DISTANCE_BETWEEN_WHEELS_HORIZONTAL = 20; //the distance between each wheel in meters - long axis
-    public static final double DISTANCE_BETWEEN_WHEELS_VERTICAL = 15; //'''''' - short axis
+    public static final double DISTANCE_BETWEEN_WHEELS_HORIZONTAL = 0.5044; //the distance between each wheel in meters - long axis
+    public static final double DISTANCE_BETWEEN_WHEELS_VERTICAL = 0.54222; //'''''' - short axis
     public static final Translation2d[] MODULE_TRANSLATIONS = new Translation2d[]{
             new Translation2d(DISTANCE_BETWEEN_WHEELS_HORIZONTAL / 2, DISTANCE_BETWEEN_WHEELS_HORIZONTAL / 2),
             new Translation2d(DISTANCE_BETWEEN_WHEELS_HORIZONTAL / 2, -DISTANCE_BETWEEN_WHEELS_HORIZONTAL / 2),
