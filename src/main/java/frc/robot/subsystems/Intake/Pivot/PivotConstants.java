9d6eb6523850ec123cf7b7050980ca0b1f32e815
Asher <smasherisaac@gmail.com>
2026-03-28 23:02:13 +0300

    Remive Todos + Spotless ~ Asher

--- src/main/java/frc/robot/subsystems/Intake/Pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/Pivot/PivotConstants.java
@@ -42,7 +42,7 @@ public class PivotConstants {
 
     public static final double SOFT_MINIMUM = PivotStates.Open.getAngle().minus(Degrees.of(5)).in(Rotations);
     public static final double SOFT_MAXIMUM = PivotStates.Closed.getAngle().plus(Degrees.of(5)).in(Rotations);
-    public static final PivotStates RESET = PivotStates.Open; //TODO: Swap to Closed
+    public static final PivotStates RESET = PivotStates.Closed;
 
     public static final PIDFGains PID_GAINS = new PIDFGains(
     600,
