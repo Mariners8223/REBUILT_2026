e2c2aa712d55019a099c6f2c978cc557ac2bf43b
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2026-07-03 10:27:47 +0300

    changes from comp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -227,10 +227,10 @@ public class RobotContainer {
         NamedCommands.registerCommand("start rollers", new InstantCommand(() -> rollers.setDutyCycle(1)));
         NamedCommands.registerCommand("stop rollers", new InstantCommand(() -> rollers.stopMotor()));
 
-        NamedCommands.registerCommand("shoot to hub", shootWithBump.get().withTimeout(5.5));
+        NamedCommands.registerCommand("shoot to hub", shootWithBump.get().withTimeout(6));
         NamedCommands.registerCommand("warm up shooter", warmupShooter.get());
         NamedCommands.registerCommand("shoot to pass", passCommand.get());
-        NamedCommands.registerCommand("aim to hub", new AimDriving(driveBase, driveController, RobotContainer::angleToHub).withTimeout(0.7));
+        NamedCommands.registerCommand("aim to hub", new AimDriving(driveBase, driveController, RobotContainer::angleToHub).withTimeout(0.5));
 
         NamedCommands.registerCommand("eject", feeder.ejectCommand());
     }
